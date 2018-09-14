41b0ad09a47976c4e650ebc338bb340f52859cce
Wez Furlong <wez@wezfurlong.org>
2018-09-14 14:19:35 -0700

    restructure ready to pull in nrf52840

--- update.sh
+++ update.sh
@@ -6,10 +6,15 @@ cargo install --force --git https://github.com/wez/svd2rust --rev e0de96e90d6fd4
 cargo install --force --version 0.99.2 rustfmt-nightly
 cargo install --force --version 0.3.0 form
 
-rm -rf src
-mkdir src
-svd2rust -i nrf52.svd --nightly
-form -i lib.rs -o src
-rm lib.rs
-cargo fmt
-rustfmt build.rs
+for chip in nrf52832 ; do
+  cd $chip
+  rm -rf src
+  mkdir src
+  ls -l ../svd/
+  svd2rust -i ../svd/$chip.svd --nightly
+  form -i lib.rs -o src
+  rm lib.rs
+  cargo fmt
+  rustfmt build.rs
+  cd ..
+done
