c3b33c852f6f13fdc4fd15b2e46c2ba50f71f69b
James Munns <james.munns@ferrous-systems.com>
2018-11-06 20:06:01 +0100

    Revert to a single chip repo

--- nrf52840/src/nfct/framestatus/mod.rs
+++
@@ -1,6 +0,0 @@
-#[doc = "Result of last incoming frame"]
-pub struct RX {
-    register: ::vcell::VolatileCell<u32>,
-}
-#[doc = "Result of last incoming frame"]
-pub mod rx;
--- nrf52840/src/ppi/fork/mod.rs
+++
@@ -1,6 +0,0 @@
-#[doc = "Description cluster[n]: Channel n task end-point"]
-pub struct TEP {
-    register: ::vcell::VolatileCell<u32>,
-}
-#[doc = "Description cluster[n]: Channel n task end-point"]
-pub mod tep;
