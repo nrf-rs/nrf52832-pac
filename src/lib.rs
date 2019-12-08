150b151a8a1569f9200a333a8cf2b1bbdc6ce50f
Jonas Schievink <jonasschievink@gmail.com>
2019-12-08 18:06:23 +0100

    Allow warnings

--- src/lib.rs
+++ src/lib.rs
@@ -1,6 +1,5 @@
 #![doc = "Peripheral access API for NRF52 microcontrollers (generated using svd2rust v0.13.1)\n\nYou can find an overview of the API [here].\n\n[here]: https://docs.rs/svd2rust/0.13.1/svd2rust/#peripheral-api"]
 #![deny(missing_docs)]
-#![deny(warnings)]
 #![allow(non_camel_case_types)]
 #![no_std]
 extern crate bare_metal;
