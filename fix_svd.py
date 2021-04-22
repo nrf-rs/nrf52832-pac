31d4bad05dc79050ee5afbcc21a7831e68b08fa9
dskleingeld <11743287+dskleingeld@users.noreply.github.com>
2021-04-22 12:45:07 +0200

    expanded python script to also add fields to EVENTS_ registers

--- fix_svd.py
+++ fix_svd.py
@@ -26,7 +26,7 @@ class TaskField:
 
 
 @dataclass
-class Task:
+class Register:
     name: str
     reg_range: Tuple[int, int]
     field: Optional[TaskField]
@@ -55,45 +55,46 @@ class Task:
         for j, line in enumerate(lines):
             if "<field>" not in line:
                 continue
-            if "<name>TASKS_" not in lines[j+1]:
+            if "<name>"+name not in lines[j+1]:
                 continue
             field = TaskField.from_lines(lines[j:])
             break
-        return Task(name, reg_range, field)
+        return Register(name, reg_range, field)
 
 
-def extract_tasks(lines: List[str]) -> List[Task]:
+def extract_registers(name_prompt: str, lines: List[str]) -> List[Register]:
     tasks = []
 
     for i, line in enumerate(lines):
-        if "<name>TASKS_" not in line:
+        # if "<name>TASKS_" not in line:
+        if name_prompt not in line:
             continue
         if "<register>" not in lines[i-1]:
             continue
 
-        task = Task.from_lines(i, lines)
+        task = Register.from_lines(i, lines)
         tasks.append(task)
 
     return tasks
 
 
-def check_task_assumptions():
+def check_assumptions(name_prompt: str):
     """ check if every TASKS_ register has a field with the same
         name that has a single bit (lsb and msb, both zero)
     """
     file = open("nrf52840.svd", "r")
     lines = file.readlines()
-    tasks = extract_tasks(lines)
+    tasks = extract_registers(name_prompt, lines)
 
     for task in tasks:
         if task.field is None:
-            print("some tasks have no task field")
+            print("some registers have no field")
             continue
 
         if task.field.lsb != 0:
-            print("not all tasks have lsb = 0")
+            print("not all registers have lsb = 0")
         if task.field.msb != 0:
-            print("not all tasks have msb = 0")
+            print("not all registers have msb = 0")
 
 
 def to_insert(taskname: str, num_spaces: int) -> List[str]:
@@ -108,21 +109,24 @@ def to_insert(taskname: str, num_spaces: int) -> List[str]:
         indent + "  </fields>\n"])
 
 
+TASK_PROMT = "<name>TASKS_"
+EVENT_PROMT = "<name>EVENTS_"
 if __name__ == "__main__":
-    check_task_assumptions()
+    check_assumptions(TASK_PROMT)
+    check_assumptions(EVENT_PROMT)
 
     file = open("nrf52832.svd", "r")
     lines = file.readlines()
-    tasks = extract_tasks(lines)
+    registers = extract_registers(TASK_PROMT, lines)
+    registers += extract_registers(EVENT_PROMT, lines)
+    registers = sorted(registers, key=lambda r: r.reg_range[0])
 
     # this assumes the fields key is not present
-    for task in reversed(tasks):
+    for task in reversed(registers):
         pos = task.reg_range[1] - 1
         reg_line = lines[task.reg_range[1]]
         indent = len(reg_line) - len(reg_line.lstrip(" "))
-        ins = to_insert(task.name, indent)
-        # print(ins)
-        lines[pos:pos] = ins
+        lines[pos:pos] = to_insert(task.name, indent)
 
     file = open("nrf52832_fixed.svd", "w")
     file.writelines(lines)
